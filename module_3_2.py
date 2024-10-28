977375ced8d66549615d73266b2bfa7ffba05a24
MalytsevaElena <malytseva.elena@gmail.com>
2024-10-28 18:02:46 +0300

    Add files via upload

---
+++ module_3_2.py
@@ -0,0 +1,20 @@
+def send_email(message,recipient,*,sender = "university.help@gmail.com"):
+    zon_recipient = recipient.split('.')
+    zon_sender = sender.split('.')
+    zon = {".com", ".ru", ".net"}
+    if ('@' not in recipient) or ('@' not in sender):
+        print('Невозможно отправить письмо с адреса', sender, 'на адрес', recipient)
+    elif ("." + zon_recipient[-1]) not in zon or ("." + zon_sender[-1]) not in zon:
+        print('Невозможно отправить письмо с адреса', sender, 'на адрес', recipient)
+    elif recipient == sender:
+        print( "Нельзя отправить письмо самому себе!")
+    elif sender == "university.help@gmail.com":
+        print("Письмо успешно отправлено с адреса", sender, "на адрес", recipient)
+    else:
+        print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса", sender, "на адрес", recipient)
+
+send_email('Это сообщение для проверки связи','vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!',
+           'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
